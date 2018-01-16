f8c2baaef43736bfcf9c0148d0abbfa056614bf7
Paul Kachur <34898444+paulkachur@users.noreply.github.com>
2018-01-15 19:33:16 -0500

    Create core.config.php

---
+++ lib/core.config.php
@@ -0,0 +1,32 @@
+<?php
+$core_var=array();
+$core_var['mysql_host']="localhost";
+$core_var['mysql_user']="xxxxxxxx";
+$core_var['mysql_password']="xxxxxxxx";
+$core_var['mysql_database']="xxxxxxxx";
+
+$core_var['timezone']="America/New_York";
+
+$core_var['do_lucan_jump']=1;
+
+$core_var['fast_levels']=array();
+$core_var['fast_levels'][0]="";
+$core_var['fast_levels'][1]="Wine &amp; Oil";
+$core_var['fast_levels'][2]="Fish, Wine &amp; Oil";
+$core_var['fast_levels'][3]="Wine &amp; Oil";
+$core_var['fast_levels'][4]="Fish, Wine &amp; Oil";
+$core_var['fast_levels'][5]="Wine";
+$core_var['fast_levels'][6]="Wine, Oil &amp; Caviar";
+$core_var['fast_levels'][7]="Meat Fast";
+$core_var['fast_levels'][8]="Strict Fast (Wine &amp; Oil)";
+$core_var['fast_levels'][9]="Strict Fast";
+$core_var['fast_levels'][10]="";
+$core_var['fast_levels'][11]="Fast Free";
+
+$core_var['fasts'][0]="";
+$core_var['fasts'][1]="Fast Day";
+$core_var['fasts'][2]="Great Lent";
+$core_var['fasts'][3]="Apostles Fast";
+$core_var['fasts'][4]="Dormition Fast";
+$core_var['fasts'][5]="Nativity Fast";
+?>
